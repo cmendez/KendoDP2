48332a62472bf7985e369ac53d51410b72376214
walter erquinigo <a20012251@gmail.com>
2013-04-10 10:51:14 -0500

    first commit, HUGE

---
+++ KendoDP2/Models/Generic/DBOject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KendoDP2.Models.Generic
+{
+    public abstract class DBOject
+    {
+        [Key]
+        public int ID { get; set; }
+        public bool IsEliminado { get; set; }
+    }
+}
