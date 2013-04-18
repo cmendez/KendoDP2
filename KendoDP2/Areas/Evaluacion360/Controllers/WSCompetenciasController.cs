254c288f962fc3a6c151a735de792cdc1f378ffc
walter erquinigo <a20012251@gmail.com>
2013-04-18 18:12:01 -0500

    agrega un webservice de prueba

---
+++ KendoDP2/Areas/Evaluacion360/Controllers/WSCompetenciasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoDP2.Areas.Evaluacion360.Controllers
+{
+    public class WSCompetenciasController : Controller
+    {
+        
+        public ActionResult Test(string nombre)
+        {
+            return Json(new { nombre = nombre + "x" }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
