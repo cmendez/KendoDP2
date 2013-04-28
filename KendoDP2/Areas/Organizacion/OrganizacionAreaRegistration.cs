118ae89c348a565ba30864ff2c607cb3f62d8acf
miguelavg <miguelvg27@gmail.com>
2013-04-28 18:30:48 -0500

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   KendoDP2/Areas/Organizacion/OrganizacionAreaRegistration.cs
    	new file:   KendoDP2/Areas/Organizacion/Views/Web.config

---
+++ KendoDP2/Areas/Organizacion/OrganizacionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace KendoDP2.Areas.Organizacion
+{
+    public class OrganizacionAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Organizacion";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Organizacion_default",
+                "Organizacion/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
