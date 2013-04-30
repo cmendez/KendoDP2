196c6c5fdefdde9aa48f6a1cf9251143d3dfee9e
walter erquinigo <a20012251@gmail.com>
2013-04-30 17:37:17 -0500

    arregla login

--- KendoDP2/Controllers/WSLoginController.cs
+++ KendoDP2/Controllers/WSLoginController.cs
@@ -17,16 +17,16 @@ namespace KendoDP2.Controllers
             return View();
         }
 
-        [AcceptVerbs(HttpVerbs.Post)]
+        [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult Login(string username, string password)
         {
             if (new DP2MembershipProvider().ValidateUser(username, password))
             {
-                return Json(1);
+                return Json(1, JsonRequestBehavior.AllowGet);
             }
             else
             {
-                return Json(0);
+                return Json(0, JsonRequestBehavior.AllowGet);
             }
         }
     }
