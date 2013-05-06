17b6a2f004fb6ee71277ece2aeddddc3b43b0d37
walter erquinigo <a20012251@gmail.com>
2013-05-06 16:54:24 -0500

    hace webservices de ever

---
+++ KendoDP2/Areas/Configuracion/Controllers/WSPeriodosController.cs
@@ -0,0 +1,22 @@
+﻿using KendoDP2.Areas.Configuracion.Models;
+using KendoDP2.Models.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoDP2.Areas.Configuracion.Controllers
+{
+    public class WSPeriodosController : Controller
+    {
+        public ActionResult ListarPeriodos()
+        {
+            using (DP2Context context = new DP2Context())
+            {
+                List<PeriodoDTO> periodos = context.TablaPeriodos.All().Select(p => p.ToDTO()).OrderByDescending(x => x.FechaInicio).ToList();
+                return Json(periodos, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ KendoDP2/Areas/Objetivos/Controllers/WSObjetivosEmpresaController.cs
@@ -0,0 +1,34 @@
+﻿using KendoDP2.Areas.Objetivos.Models;
+using KendoDP2.Models.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoDP2.Areas.Objetivos.Controllers
+{
+    public class WSObjetivosEmpresaController : Controller
+    {
+        public ActionResult ListarObjetivosEmpresa(int tipoObjetivoBSCID, int BSCID)
+        {
+            using (DP2Context context = new DP2Context())
+            {
+                return Json(context.TablaObjetivos.Where(o => o.TipoObjetivoBSCID == tipoObjetivoBSCID && o.BSCID == BSCID).Select(o => o.ToDTO()), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CrearObjetivoEmpresa(ObjetivoDTO objetivo)
+        {
+            using (DP2Context context = new DP2Context())
+            {
+                Objetivo o = new Objetivo(objetivo);
+                o.Creador = context.TablaColaboradores.One(c => c.Username.Equals(User.Identity.Name));
+                context.TablaObjetivos.AddElement(o);
+                return Json(new { success = true });
+            }
+        }
+
+    }
+}
