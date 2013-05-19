bf1c8922e38e2bdcb7ac664695c8faa24b3c147d
walter erquinigo <a20012251@gmail.com>
2013-05-19 15:46:42 -0500

    arregla intranet

--- KendoDP2/Areas/Organizacion/Controllers/IntranetController.cs
+++ KendoDP2/Areas/Organizacion/Controllers/IntranetController.cs
@@ -22,6 +22,13 @@ namespace KendoDP2.Areas.Organizacion.Controllers
             using (DP2Context context = new DP2Context())
             {
                 int ColaboradorID = DP2MembershipProvider.GetPersonaID(this);
+                ViewBag.ColaboradorDTO = context.TablaColaboradores.FindByID(ColaboradorID).ToDTO();
+                ViewBag.tipoDocumentos = context.TablaTiposDocumentos.All().Select(c => c.ToDTO()).ToList();
+                ViewBag.gradoAcademico = context.TablaGradosAcademicos.All().Select(c => c.ToDTO()).ToList();
+                ViewBag.estadosColaborador = context.TablaEstadosColaboradores.All().Select(c => c.ToDTO()).ToList();
+                ViewBag.pais = context.TablaPaises.All().Select(c => c.ToDTO()).ToList();
+                ViewBag.areas = context.TablaAreas.All().Select(c => c.ToDTO()).ToList();
+                ViewBag.puestos = context.TablaPuestos.All().Select(c => c.ToDTO()).ToList();
                 return View();
             }
             
