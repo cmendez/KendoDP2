c815a2d91c215502ab878d211d9b5146bc3b5d62
Manuel Solorzano <manuel.solorzanoc@gmail.com>
2013-05-19 18:40:12 -0500

    Creacion clase Postulante en Reclutamiento
    Signed-off-by: Manuel Solorzano <manuel.solorzanoc@gmail.com>

---
+++ KendoDP2/Areas/Reclutamiento/Models/Postulante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KendoDP2.Areas.Organizacion.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace KendoDP2.Areas.Reclutamiento.Models
+{
+    public class Postulante : Persona
+    {
+        public string estado;
+    }
+}
