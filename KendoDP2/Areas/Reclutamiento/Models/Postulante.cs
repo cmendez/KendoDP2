be2ff6ec7ccfd3da08c8d61878c0b458e38367aa
Manuel Solorzano <manuel.solorzanoc@gmail.com>
2013-05-19 19:37:01 -0500

    Avazando Postulante
    Signed-off-by: Manuel Solorzano <manuel.solorzanoc@gmail.com>

--- KendoDP2/Areas/Reclutamiento/Models/Postulante.cs
+++ KendoDP2/Areas/Reclutamiento/Models/Postulante.cs
@@ -9,6 +9,44 @@ namespace KendoDP2.Areas.Reclutamiento.Models
 {
     public class Postulante : Persona
     {
-        public string estado;
+        public string Estado { get; set; }
+        public virtual ICollection<OfertaLaboral> OfertasPostuladas { get; set; }
+
+        public Postulante() { }
+
+        public Postulante(PostulanteDTO p)
+        {
+            LoadFromDTO(p);
+        }
+
+        public Postulante LoadFromDTO(PostulanteDTO p)
+        {
+            ID = p.ID;
+            Estado = p.Estado;
+            return this;
+        }
+
+        public PostulanteDTO ToDTO()
+        {
+            return new PostulanteDTO(this);
+        }
+
+    }
+
+    public class PostulanteDTO
+    {
+        [ScaffoldColumn(false)]
+        public int ID { get; set; }
+        public string Estado { get; set; }
+        public string NombreCompleto { get; set; }
+
+
+        public PostulanteDTO() { }
+        public PostulanteDTO(Postulante p)
+        {
+            ID = p.ID;
+            Estado = p.Estado;
+            NombreCompleto = p.ApellidoPaterno + " " + p.ApellidoMaterno + ", " + p.Nombres;
+        }
     }
 }
