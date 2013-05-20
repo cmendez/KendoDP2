3f51c33b78015ab69e9335a36f822e65ac27ef5a
Manuel Solorzano <manuel.solorzanoc@gmail.com>
2013-05-19 20:33:46 -0500

    WS de oferta laboral - correcion
    Signed-off-by: Manuel Solorzano <manuel.solorzanoc@gmail.com>

---
+++ KendoDP2/Areas/Reclutamiento/Controllers/WSOfertaLaboralController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoDP2.Areas.Reclutamiento.Controllers
+{
+    public class WSOfertaLaboralController : Controller
+    {
+        //
+        // GET: /Reclutamiento/WSOfertaLaboral/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
