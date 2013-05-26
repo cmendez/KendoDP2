e2a0679718fc4754ec4f785a8401ac91cbc19806
katherine tucto <ktucto@gmail.com>
2013-05-26 15:15:37 -0500

    agregue files de evaluaciones

---
+++ KendoDP2/Areas/Evaluacion360/Controllers/EvaluacionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoDP2.Areas.Evaluacion360.Controllers
+{
+    [Authorize()]
+    public class EvaluacionController : Controller
+    {
+        //
+        // GET: /Evaluacion360/Evaluacion/
+        public EvaluacionController()
+            : base()
+        {
+            ViewBag.Area = "Evaluacion360";
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
