965c58f64db98e27493e70c9b1e76eb36278453f
walter erquinigo <a20012251@gmail.com>
2013-06-02 14:52:46 -0500

    fix ws de objetivos de subordinados

--- KendoDP2/Areas/Objetivos/Controllers/WSObjetivosSubordinadosController.cs
+++ KendoDP2/Areas/Objetivos/Controllers/WSObjetivosSubordinadosController.cs
@@ -19,8 +19,10 @@ namespace KendoDP2.Areas.Objetivos.Controllers
                 int puestoID = context.TablaColaboradores.FindByID(idColaborador).ToDTO().PuestoID;
                 Puesto puesto = context.TablaPuestos.FindByID(puestoID);
                 List<Objetivo> objetivosPuesto = puesto.Objetivos.Where(x => x.GetBSCIDRaiz(context) == idPeriodo).ToList();
+                List<Objetivo> objetivosHijos = new List<Objetivo>();
+                objetivosPuesto.ForEach(x => objetivosHijos.AddRange(x.ObjetivosHijos.ToList()));
                 List<Objetivo> objetivos = new List<Objetivo>();
-                objetivosPuesto.ForEach(x => objetivos.AddRange(x.ObjetivosHijos.ToList()));
+                objetivosHijos.ForEach(x => objetivos.AddRange(x.ObjetivosHijos.Where(a => a.IsObjetivoIntermedio).ToList()));
                 return Json(objetivos.Select(c => c.ToDTO(context)).ToList(), JsonRequestBehavior.AllowGet);
             }
         }
