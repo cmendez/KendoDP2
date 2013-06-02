945509c499577512c60de45ea7ba389a9587ff6c
walter erquinigo <a20012251@gmail.com>
2013-06-02 16:06:12 -0500

    fix

--- KendoDP2/Areas/Evaluacion360/Models/Examen.cs
+++ KendoDP2/Areas/Evaluacion360/Models/Examen.cs
@@ -22,7 +22,7 @@ namespace KendoDP2.Areas.Evaluacion360.Models
         //public DateTime? FechaCierre { get; set; }
 
         public int EvaluadorID { get; set; }
-        public virtual Evaluador evaluador { get; set; }
+        //public virtual Evaluador evaluador { get; set; }
 
         public int EstadoEvaluacionID { get; set; }
         public virtual EstadoColaboradorXProcesoEvaluacion Estado { get; set; }
