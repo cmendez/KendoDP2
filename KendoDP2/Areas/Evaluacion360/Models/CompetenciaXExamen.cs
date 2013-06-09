ca32851de24d3f232480618b6692c09538692901
katherine tucto <ktucto@gmail.com>
2013-06-09 18:24:43 -0500

    agregue files

---
+++ KendoDP2/Areas/Evaluacion360/Models/CompetenciaXExamen.cs
@@ -0,0 +1,21 @@
+﻿using KendoDP2.Models.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoDP2.Areas.Evaluacion360.Models
+{
+    public class CompetenciaXExamen : DBObject
+    {
+
+        public int CompetenciaID { get; set; }
+        public int Nota { get; set; }
+        public int ExamenID { get; set; }
+        public String Descripcion { get; set; }
+        public virtual ICollection<Pregunta> ListaPreguntas { get; set; }
+
+        public CompetenciaXExamen() { 
+        }
+    }
+}
