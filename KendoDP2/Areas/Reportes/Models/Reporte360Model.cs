8a31b1a95921fec048e5c00ac7b57daf7cf8583e
Christian Perez <cperezortiz.90@gmail.com>
2013-06-20 11:38:34 -0500

    WS Reporte 360:
    -Primer reporte presentable.

--- KendoDP2/Areas/Reportes/Models/Reporte360Model.cs
+++ KendoDP2/Areas/Reportes/Models/Reporte360Model.cs
@@ -14,6 +14,9 @@ namespace KendoDP2.Areas.Reportes.Models
     public class Reporte360Model
     {
         public const string JEFE = "Jefe";
+        public const string COMPANHERO = "Compañeros pares";
+        public const string SUBORDINADO = "Subordinados";
+        public const string MISMO = "Él mismo";
     }
 
     public class ProcesoReportadoDTO
@@ -35,29 +38,100 @@ namespace KendoDP2.Areas.Reportes.Models
 
         public static ICollection<NotaXTipoEvaluadorDTO> ListaNotasParcialesToDTO(ICollection<Evaluador> evaluadores)
         {
+            //en la lista evaluadores, se tienen todas las evaluaciones del colaborador en un solo proceso
             List<NotaXTipoEvaluadorDTO> ListaNotasDTO = new List<NotaXTipoEvaluadorDTO>();
-            NotaXTipoEvaluadorDTO notaParcial;
+            NotaXTipoEvaluadorDTO notaParcial = null;
 
             //Jefes:
             DP2Context context = new DP2Context();
-            notaParcial = new NotaXTipoEvaluadorDTO();
             foreach (Evaluador evaluador in evaluadores)
             {
                 var jefeAux = GestorServiciosPrivados.consigueElJefe(evaluador.ElEvaluado, context);
                 if (jefeAux != null)
+                {
                     if (jefeAux.ID == evaluador.ElIDDelEvaluador)
                     {
-                        notaParcial.notaParcial = context.TablaExamenes.One(a=>a.EvaluadorID == evaluador.ID).NotaExamen;
+                        notaParcial = new NotaXTipoEvaluadorDTO();
+                        notaParcial.notaParcial = context.TablaExamenes.One(a => a.EvaluadorID == evaluador.ID).NotaExamen;
                         notaParcial.tipoEvaluador = Reporte360Model.JEFE;
+                        ListaNotasDTO.Add(notaParcial);
                         break;
                     }
+                }
             }
-            ListaNotasDTO.Add(notaParcial);
+
 
             //Compañeros Pares:
+            int notaSuma = 0;
+            int count = 0;
+            int notaPromedio = 0;
+            foreach (Evaluador evaluador in evaluadores)
+            {
+                var companheros = GestorServiciosPrivados.consigueSusCompañerosPares(evaluador.ElEvaluado, context);
+                if (companheros != null)
+                {
+                    foreach (Colaborador companhero in companheros)
+                    {
+                        if (companhero.ID == evaluador.ElIDDelEvaluador && companhero.ID != evaluador.ElEvaluado)
+                        {
+                            count++;
+                            notaSuma += context.TablaExamenes.One(a => a.EvaluadorID == evaluador.ID).NotaExamen; 
+                            break;
+                        }
+                    }
+                }
+            }
+            if (count != 0)
+            {
+                notaPromedio = notaSuma / count;
+                notaParcial = new NotaXTipoEvaluadorDTO();
+                notaParcial.notaParcial = notaPromedio;
+                notaParcial.tipoEvaluador = Reporte360Model.COMPANHERO;
+                ListaNotasDTO.Add(notaParcial);
+            }
 
 
             //Subordinados:
+            notaSuma = 0;
+            count = 0;
+            notaPromedio = 0;
+            foreach (Evaluador evaluador in evaluadores)
+            {
+                var subordinados = GestorServiciosPrivados.consigueSusSubordinados(evaluador.ElEvaluado, context);
+                if (subordinados != null)
+                {
+                    foreach (Colaborador subordinado in subordinados)
+                    {
+                        if (subordinado.ID == evaluador.ElIDDelEvaluador)
+                        {
+                            count++;
+                            notaSuma += context.TablaExamenes.One(a => a.EvaluadorID == evaluador.ID).NotaExamen;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (count != 0)
+            {
+                notaPromedio = notaSuma / count;
+                notaParcial = new NotaXTipoEvaluadorDTO();
+                notaParcial.notaParcial = notaPromedio;
+                notaParcial.tipoEvaluador = Reporte360Model.SUBORDINADO;
+                ListaNotasDTO.Add(notaParcial);
+            }
+
+            //El mismo:
+            foreach (Evaluador evaluador in evaluadores)
+            {
+                if (evaluador.ElEvaluado == evaluador.ElIDDelEvaluador)
+                {
+                    notaParcial = new NotaXTipoEvaluadorDTO();
+                    notaParcial.notaParcial = context.TablaExamenes.One(a => a.EvaluadorID == evaluador.ID).NotaExamen;
+                    notaParcial.tipoEvaluador = Reporte360Model.MISMO;
+                    ListaNotasDTO.Add(notaParcial);
+                    break;
+                }
+            }
 
             return ListaNotasDTO;
         }
