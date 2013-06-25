a9f9ba6014e93e63fa0c68f2291e6ea660a28d7a
walter erquinigo <a20012251@gmail.com>
2013-06-24 20:10:47 -0500

    bug

--- KendoDP2/Areas/Reclutamiento/Controllers/OfertasLaboralesExternasController.cs
+++ KendoDP2/Areas/Reclutamiento/Controllers/OfertasLaboralesExternasController.cs
@@ -150,71 +150,78 @@ namespace KendoDP2.Areas.Reclutamiento.Controllers
                 OfertaLaboralXPostulante postulanteOferta = oferta.Postulantes.Where(p => p.ID == postulanteXOfertaLaboralID).FirstOrDefault();
                 MiscController controladorGeneral = new MiscController();
                 var org = context.TablaOrganizaciones.All().FirstOrDefault();
-                // aca asignar el nuevo puesto
-                int puestoPadre = oferta.PuestoID;
-                List<PuestoDTO> puestos = BuscoPuestoPapaEHijos(puestoPadre);
-
-                if (puestos != null)
+                if (postulanteOferta.EstadoPostulantePorOferta.Descripcion.Equals("Aprobado Fase 3"))
                 {
-                    PuestoDTO puestoEncontrado = BuscaPuestoLibreAsignar(puestos);
+                    // aca asignar el nuevo puesto
+                    int puestoPadre = oferta.PuestoID;
+                    List<PuestoDTO> puestos = BuscoPuestoPapaEHijos(puestoPadre);
 
-                    if (puestoEncontrado != null)
+                    if (puestos != null)
                     {
+                        PuestoDTO puestoEncontrado = BuscaPuestoLibreAsignar(puestos);
 
-                        ColaboradorDTO colaboradorDTO = new ColaboradorDTO
+                        if (puestoEncontrado != null)
                         {
-                            Nombre = postulanteOferta.Postulante.Nombres,
-                            ApellidoPaterno = postulanteOferta.Postulante.ApellidoPaterno,
-                            ApellidoMaterno = postulanteOferta.Postulante.ApellidoMaterno,
-                            TipoDocumentoID = postulanteOferta.Postulante.TipoDocumentoID,
-                            NumeroDocumento = postulanteOferta.Postulante.NumeroDocumento,
-                            CorreoElectronico = postulanteOferta.Postulante.CorreoElectronico,
-                            CentroEstudios = postulanteOferta.Postulante.CentroEstudios,
-                            GradoAcademicoID = (int)postulanteOferta.Postulante.GradoAcademicoID,
-                            CurriculumVitaeID = postulanteOferta.Postulante.CurriculumVitaeID,
-                            //AreaID = oferta.AreaID,
-                            //PuestoID = oferta.PuestoID,
 
-                            Sueldo = oferta.SueldoTentativo
+                            ColaboradorDTO colaboradorDTO = new ColaboradorDTO
+                            {
+                                Nombre = postulanteOferta.Postulante.Nombres,
+                                ApellidoPaterno = postulanteOferta.Postulante.ApellidoPaterno,
+                                ApellidoMaterno = postulanteOferta.Postulante.ApellidoMaterno,
+                                TipoDocumentoID = postulanteOferta.Postulante.TipoDocumentoID,
+                                NumeroDocumento = postulanteOferta.Postulante.NumeroDocumento,
+                                CorreoElectronico = postulanteOferta.Postulante.CorreoElectronico,
+                                CentroEstudios = postulanteOferta.Postulante.CentroEstudios,
+                                GradoAcademicoID = (int)postulanteOferta.Postulante.GradoAcademicoID,
+                                CurriculumVitaeID = postulanteOferta.Postulante.CurriculumVitaeID,
+                                //AreaID = oferta.AreaID,
+                                //PuestoID = oferta.PuestoID,
 
-                        };
+                                Sueldo = oferta.SueldoTentativo
 
-                        Colaborador c = new Colaborador(colaboradorDTO);
-                        c.EstadoColaborador = context.TablaEstadosColaboradores.One(x => x.Descripcion.Equals("Contratado"));
-                        context.TablaColaboradores.AddElement(c);
-                        Puesto pe = context.TablaPuestos.FindByID(puestoEncontrado.ID);
-                        //hasta mientras fecha de ingreso = datetime.now
-                        ColaboradorXPuesto cruce = new ColaboradorXPuesto { ColaboradorID = c.ID, PuestoID = pe.ID, Sueldo = colaboradorDTO.Sueldo, FechaIngresoPuesto = DateTime.Now };
-                        context.TablaColaboradoresXPuestos.AddElement(cruce);
+                            };
 
-                        postulanteOferta.EstadoPostulantePorOferta = context.TablaEstadoPostulanteXOferta.One(p => p.Descripcion.Equals("Contratado"));
-                        context.TablaOfertaLaboralXPostulante.ModifyElement(postulanteOferta);
+                            Colaborador c = new Colaborador(colaboradorDTO);
+                            c.EstadoColaborador = context.TablaEstadosColaboradores.One(x => x.Descripcion.Equals("Contratado"));
+                            context.TablaColaboradores.AddElement(c);
+                            Puesto pe = context.TablaPuestos.FindByID(puestoEncontrado.ID);
+                            //hasta mientras fecha de ingreso = datetime.now
+                            ColaboradorXPuesto cruce = new ColaboradorXPuesto { ColaboradorID = c.ID, PuestoID = pe.ID, Sueldo = colaboradorDTO.Sueldo, FechaIngresoPuesto = DateTime.Now };
+                            context.TablaColaboradoresXPuestos.AddElement(cruce);
 
-                        if (postulanteOferta.Postulante.CorreoElectronico != null)
-                        {
-                            controladorGeneral.SendEmail(postulanteOferta.Postulante.CorreoElectronico, "[" + org.RazonSocial + "] Aviso de Seleccion",
-                                    RetornaMensajeContrato(postulanteOferta.Postulante.ToDTO().NombreCompleto, postulanteOferta.OfertaLaboral.Area.Nombre, postulanteOferta.OfertaLaboral.Puesto.Nombre));
+                            postulanteOferta.EstadoPostulantePorOferta = context.TablaEstadoPostulanteXOferta.One(p => p.Descripcion.Equals("Contratado"));
+                            context.TablaOfertaLaboralXPostulante.ModifyElement(postulanteOferta);
 
+                            if (postulanteOferta.Postulante.CorreoElectronico != null)
+                            {
+                                controladorGeneral.SendEmail(postulanteOferta.Postulante.CorreoElectronico, "[" + org.RazonSocial + "] Aviso de Seleccion",
+                                        RetornaMensajeContrato(postulanteOferta.Postulante.ToDTO().NombreCompleto, postulanteOferta.OfertaLaboral.Area.Nombre, postulanteOferta.OfertaLaboral.Puesto.Nombre));
+
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("Alerta", "Se selecciona y cambia el puesto del postulante, pero no se envía la notificación. Revise los datos e intente comunicarse por otro medio");
+                            }
+
+                            return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
                         }
                         else
                         {
-                            ModelState.AddModelError("Alerta", "Se selecciona y cambia el puesto del postulante, pero no se envía la notificación. Revise los datos e intente comunicarse por otro medio");
+                            ModelState.AddModelError("", "Este puesto está ocupado, por favor revisar el contrato del colaborador en el puesto actual o si existe dentro de la estructura");
+                            return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
                         }
-
-                        return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
                     }
                     else
                     {
-                        ModelState.AddModelError("", "Este puesto está ocupado, por favor revisar el contrato del colaborador en el puesto actual o si existe dentro de la estructura");
-                        return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
+                        ModelState.AddModelError("", "No existe el puesto");
                     }
+                    return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
                 }
                 else
-                {
-                    ModelState.AddModelError("", "No existe el puesto");
+                { 
+                    ModelState.AddModelError("", "El postulante ya fue contratado o rechazado para esta oferta laboral.");
+                    return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
                 }
-                return Json(new[] { postulanteOferta.ToDTO() }.ToDataSourceResult(request, ModelState), JsonRequestBehavior.AllowGet);
-
             }
 
         }
